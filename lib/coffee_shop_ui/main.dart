d1ba23678f8a0bb58e5b97a7ff1ca54fbbeda719
kostevg871 <kostyrkoevgeniy@yandex.ru>
2021-12-15 20:28:02 +0300

    Datils page add

--- lib/coffee_shop_ui/main.dart
+++ lib/coffee_shop_ui/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apps/coffee_shop_ui/details_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -124,8 +125,59 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
         SizedBox(height: 15.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Explorer nearby',
+              style: TextStyle(
+                fontFamily: 'valera',
+                fontSize: 17,
+                color: Color(0xFF473D3A),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 15),
+              child: Text(
+                "See All",
+                style: TextStyle(
+                  fontFamily: 'valera',
+                  fontSize: 15,
+                  color: Color(0xFFCEC7C4),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 15),
+        Container(
+          height: 125,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              _buildImage('assets/coffee_shop/coffee.jpg'),
+              _buildImage('assets/coffee_shop/coffee2.jpg'),
+              _buildImage('assets/coffee_shop/coffee3.jpg'),
+            ],
+          ),
+        ),
+        SizedBox(height: 20),
       ],
     ));
+  }
+
+  _buildImage(String imgPath) {
+    return Padding(
+      padding: EdgeInsets.only(right: 10),
+      child: Container(
+        height: 100,
+        width: 175,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: DecorationImage(image: AssetImage(imgPath), fit: BoxFit.cover),
+        ),
+      ),
+    );
   }
 
   _coffeeListCard(String imgPath, String coffeeName, String shopName,
@@ -152,9 +204,111 @@ class _MyHomePageState extends State<MyHomePage> {
                       borderRadius: BorderRadius.circular(25),
                       color: Color(0xFFDAB68C),
                     ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 60),
+                        Text(
+                          shopName + '\'s',
+                          style: TextStyle(
+                              fontFamily: 'nunito',
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          coffeeName,
+                          style: TextStyle(
+                            fontFamily: 'valera',
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          description,
+                          style: TextStyle(
+                            fontFamily: 'nunito',
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              price,
+                              style: TextStyle(
+                                fontFamily: 'valera',
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF4A4742),
+                              ),
+                            ),
+                            Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.white,
+                              ),
+                              child: Center(
+                                child: Icon(
+                                  Icons.favorite,
+                                  color: isFavorite ? Colors.red : Colors.grey,
+                                  size: 15,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 60,
+                  top: 25,
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      image: AssetImage(imgPath),
+                      fit: BoxFit.contain,
+                    )),
                   ),
                 ),
               ],
+            ),
+            SizedBox(height: 20),
+            InkWell(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => DetailsPage()));
+              },
+              child: Container(
+                height: 50,
+                width: 225,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: Color(0xFF473D3A),
+                ),
+                child: Center(
+                  child: Text(
+                    "Order Now",
+                    style: TextStyle(
+                      fontFamily: 'nunito',
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
